4ae1d73a3d12641b062664f7706a623472dab660
R_amini <R_amini@DESKTOP-ET9SFRB>
2023-05-28 09:54:34 +0430

    all

--- ConsoleApp01/Program.cs
+++ ConsoleApp01/Program.cs
@@ -1,14 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 {
 
-     int a, b, c;
+    int a, b, c;
     a = int.Parse(Console.ReadLine());
     b = int.Parse(Console.ReadLine());
 
     c = a + b;
 
-   Console.ReadKey();
 
-     }
+    Console.WriteLine(c);
 
-Console.WriteLine(c);
+    Console.ReadKey();
+
+    
+}
