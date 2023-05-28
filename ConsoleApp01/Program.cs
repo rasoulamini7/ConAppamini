83eb50bb77739ab937ea17389d1d859dfcd678de
R_amini <R_amini@DESKTOP-ET9SFRB>
2023-05-28 09:24:26 +0430

    Sum

--- ConsoleApp01/Program.cs
+++ ConsoleApp01/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+{
+
+     int a, b, c;
+    a = int.Parse(Console.ReadLine());
+    b = int.Parse(Console.ReadLine());
+
+    c = a + b;
+
+   Console.ReadKey();
+
+     }
